1bbfa9e1631d784bf0071bafd39d54448023ea1e
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-10-30 23:23:33 +0300

    Create a method to configure api key and urls.

--- app/requests.py
+++ app/requests.py
@@ -1,3 +1,19 @@
 import urllib.request,json
 from .models import News,Articles
 from datetime import date
+
+#Getting the api key
+api_key = None
+
+#Getting News base url
+base_url = None
+
+#Getting Articles url
+articles_url = None
+
+def configure_request(app):
+    global api_key,base_url,articles_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['SOURCE_BASE_URL']
+    articles_url = app.config['ARTICLE_BASE_URL']
+
