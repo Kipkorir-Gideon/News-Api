d09567f8e2e21156a0a0676a6f8f973f336d06d1
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-10-30 22:57:29 +0300

    Add Articles class and initialize it.

--- app/models.py
+++ app/models.py
@@ -8,3 +8,17 @@ class News:
         self.description = description
         self.url = url
         self.category = category
+
+
+class Articles:
+    '''
+    A class for articles objects
+    '''
+    def __init__(self,id,author,title,description,url,image,date):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image = image
+        self.date = date
