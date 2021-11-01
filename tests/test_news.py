11f7a02afb819693ccca5d40491839873275a285
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-01 09:49:15 +0300

    Add news test.

---
+++ tests/test_news.py
@@ -0,0 +1,23 @@
+import unittest
+from app.models import News
+
+class SourcesTest(unittest.TestCase):
+    '''
+    Test Class to test the behavior of the News class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.news = News('Yuri1','Yuri News','Facebook changed its parent name to Meta.','https://www.yurinews.com/section/news','technology')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.news,News))
+
+    def test_to_check_instance_variables(self):
+        self.assertEquals(self.new_source.id,'Yuri1')
+        self.assertEquals(self.new_source.name,'Yuri News')
+        self.assertEquals(self.new_source.description,'Facebook changed its parent name to Meta.')
+        self.assertEquals(self.new_source.url,'https://www.yurinews.com/section/news')
+        self.assertEquals(self.new_source.category,'technology')
